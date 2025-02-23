100e357874955658e15655e6a6a7ab1f892ee588
Roshan Gauchan <rgauchan3@gmail.com>
2025-02-23 16:10:25 +0000

    serving index.html to the server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT,()=>{
-    console.log(`Server Started on ${PORT}`);
-})
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+
+console.log(dirname(fileURLToPath(import.meta.url)));
+
