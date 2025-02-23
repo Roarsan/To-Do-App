a3f275415348f5789a37cbdee9d12db3e31ab7cc
Roshan Gauchan <rgauchan3@gmail.com>
2025-02-23 16:17:09 +0000

    setting up db next

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// stores the cuurent the path of the current directory
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//serves the static files from public folder to the server
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 
-console.log(dirname(fileURLToPath(import.meta.url)));
+
 
